ddc745216fdf780fe3c04d084325aa2aef73500f
Beau Winiger <bcwiniger@gmail.com>
2018-02-20 21:33:55 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stravapush
-Small script to push my strava files up to Strava
+This is a simple script to upload a file to your Strava profile
+so I don't have to do it by hand anymore. 
+
+###More stuff coming down here
---
+++ main.py
@@ -0,0 +1,14 @@
+!#/usr/bin/python
+
+    def main():
+        #get the latest file from the folder
+
+        #get strava auth token from env variable
+
+        #push file to strava
+
+        #wait for response from Strava
+
+        #report back success or error
+
+main
